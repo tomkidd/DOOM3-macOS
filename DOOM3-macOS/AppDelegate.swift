b319d3c53b31dcfc62c9f9a5d5e617fc148aabeb
Tom Kidd <tomkidd@gmail.com>
2019-02-20 16:47:19 -0600

    Added barebones Mac project

---
+++ DOOM3-macOS/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  DOOM3-macOS
+//
+//  Created by Tom Kidd on 2/20/19.
+//  Copyright © 2019 Tom Kidd. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
